73460de1aaf286bef14a5b9ac1bba75057b748f2
Geoffrey009 <amazwhule999@gmail.com>
2025-09-17 00:24:41 +0100

    regexp issue

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/{*any}", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => res.send("Hello world"));
 
 app.listen(5000, () => console.log("✅ Server running on 5000"));
